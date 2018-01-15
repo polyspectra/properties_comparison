4ba6a96d5f219a8c61fa4d12120829b500a5ae19
Sam Bozek <sambozek@gmail.com>
2018-01-15 11:37:35 -0800

    Procfile app changed to normal

--- app.py
+++ app.py
@@ -100,5 +100,5 @@ def update_graph(xaxis_column_name, yaxis_column_name, Material_Class
     }
 
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
